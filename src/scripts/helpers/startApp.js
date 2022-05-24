3c8a9859fbb9e2501da2b52f3034c43d01117353
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-05-23 21:31:16 -0500

    worked on in class assignment

--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -1,6 +1,8 @@
+import { getBooks } from '../../api/bookData';
 import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/domBuilder';
 import navBar from '../components/navBar';
+import { showBooks } from '../components/pages/books';
 import domEvents from '../events/domEvents';
 import formEvents from '../events/formEvents';
 import navigationEvents from '../events/navigationEvents';
@@ -12,8 +14,8 @@ const startApp = () => {
   navBar(); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
-
   // TODO: Put all books on the DOM on App load
+  getBooks().then((booksArray) => showBooks(booksArray));
 };
 
 export default startApp;
